c0bd4c5ab79e1cd9f5a24ebe78b0f7b981fc0dcd
Arthur <art1choke@yandex.com>
2023-01-11 17:13:29 +0300

    removed all alerts, changed initialisation var. movieTitle and score

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 'use strict';
-alert('Сколько фильмов вы уже посмотрели?');
-const numberOfFilms = +prompt('', '3');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '3');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,9 +10,10 @@ const personalMovieDB = {
 };
 
 for (let i = 0; i <= 2; i++) {
-    alert('Один из последних просмотренных фильмов?');
-    let movieTitle = prompt();
-    alert('На сколько оцените его?');
-    let score = +prompt();
+    let movieTitle = 
+    prompt('Один из последних просмотренных фильмов?', 'Logan'),
+    score = +prompt('На сколько оцените его?', '8.1');
     personalMovieDB.movies[movieTitle] = score;
 }
+
+console.log(personalMovieDB);
